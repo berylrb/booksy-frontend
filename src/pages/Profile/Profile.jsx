82c66a2f5cc82b1a05d486b97303f9a180be67c5
Beryl B <beriebaldwin@gmail.com>
2023-02-01 19:01:04 -0500

    create simple conditional view in profile show page

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,12 +1,41 @@
 import styles from './Profile.module.css'
+import { useState, useEffect } from "react"
+import { useParams } from 'react-router-dom'
+import * as profileService from '../../services/profileService'
+
 
 const Profile = ({ user }) => {
+  const { id } = useParams()
+
+  const [profile, setProfile] = useState()
+
+
+  useEffect(() => {
+    const fetchProfile = async () => {
+      const profileData = await profileService.show(id)
+      setProfile(profileData)
+    }
+    fetchProfile()
+  }, [id])
+
+  console.log(profile, 'profile')
 
   return (
     <>
-      <main>
-        <div className={styles.tempDiv}>
-          <p>my profile</p>
+      <main className={styles.profileMain}>
+        <div className={styles.greetDiv}>
+          {user?.profile === profile?._id ?
+            <>
+              <p>My Profile</p>
+              <img className={styles.profileImg} src={profile?.photo} alt="my profile pic" />
+            </>
+            :
+            <>
+              <p>{profile?.name}'s Profile</p>
+              <img className={styles.profileImg} src={profile?.photo} alt="my profile pic" />
+            </>
+        }
+
         </div>
       </main>
     </>
--- src/pages/Profiles/Profiles.jsx
+++ src/pages/Profiles/Profiles.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import * as profileService from '../../services/profileService'
+import { Link } from 'react-router-dom'
 
 const Profiles = () => {
   const [profiles, setProfiles] = useState([])
@@ -11,21 +12,27 @@ const Profiles = () => {
     }
     fetchProfiles()
   }, [])
+  console.log(profiles, 'profiles')
 
   return (
     <>
       <h1>Hello. This is a list of all the profiles.</h1>
-      {profiles.length ? 
+      {profiles.length ?
         <>
           {profiles.map(profile =>
-            <p key={profile._id}>{profile.name}</p>
+            <Link
+              key={profile.id}
+              to={`/profiles/${profile._id}`}
+            >
+              <p key={profile._id}>{profile.name}</p>
+            </Link>
           )}
         </>
-      :
+        :
         <p>No profiles yet</p>
       }
     </>
   )
 }
- 
+
 export default Profiles
