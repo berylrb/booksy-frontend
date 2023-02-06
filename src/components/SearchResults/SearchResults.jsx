f7a29e4d73d37aea7b91240f22a66d317fd11b1c
Beryl B <beriebaldwin@gmail.com>
2023-02-06 01:09:46 -0500

    start search results component

---
+++ src/components/SearchResults/SearchResults.jsx
@@ -0,0 +1,16 @@
+import styles from './SearchResults.module.css'
+import { useParams } from 'react-router-dom'
+import { useState, useEffect } from 'react';
+
+const SearchResults = (props) => {
+  const [searchDetails, setSearchDetails] = useState({})
+
+  
+  return (
+    <>
+      
+    </>
+  );
+}
+
+export default SearchResults;
