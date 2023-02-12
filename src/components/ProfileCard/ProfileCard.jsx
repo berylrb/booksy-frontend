597608a39cc752f451a25202b0d1acc2cecf816c
Beryl B <beriebaldwin@gmail.com>
2023-02-12 14:00:35 -0500

    set profile card bg with complementary color

--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -1,14 +1,24 @@
 import styles from './ProfileCard.module.css'
 import { Link } from 'react-router-dom'
 import Avatar from 'react-nice-avatar'
+import compColors from 'complementary-colors'
 
 const ProfileCard = ({ profile }) => {
   const avConfig = profile?.photo
+  const cardBgColor = new compColors(profile?.photo.bgColor)
+  const color = cardBgColor.tetradic()
+  const newColor = `rgba(${color[1].r}, ${color[1].g}, ${color[1].b}, 0.4)`
+
+  const cardBg = {
+    background: `linear-gradient(to bottom, ${newColor} 0%, ${newColor} 65%, rgba(247, 246, 243) 65%, rgba(247, 246, 243) 100%)`
+  }
+
   return (
     <>
       <Link className={styles.profileLink}
       style={{textDecoration: 'none'}} to={`/profiles/${profile._id}`} >
-        <article className={styles.profCardContainer}>
+        <article style={cardBg}
+        className={styles.profCardContainer}>
           <div className={styles.colorBlock}></div>
           <span className={styles.picNameSpan}>
             <div className={styles.imgDiv}>
